88e8eab610c259ad9d03fe24a7085bf1790324b6
Andrew Kang <andrewsjkang@gmail.com>
2018-08-27 12:06:36 -0700

    Fixes ESlinting errors to support JSX

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 class App extends React.Component {
-  handleTitleClick() {
-    alert("you clicked on the title");
-  }
   render() {
     return (
       <div>
-        <h1 onClick={this.handleTitleClick}>Adopt Me!</h1>
+        <h1>Adopt Me!</h1>
         <Pet name="Luna" animal="Dog" breed="Havanese" />
         <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
         <Pet name="Doink" animal="Cat" breed="Mix" />
