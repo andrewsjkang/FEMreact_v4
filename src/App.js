28256326c6ecd04526f9151eb91be5d4115d1de8
Andrew Kang <andrewsjkang@gmail.com>
2018-08-27 09:45:41 -0700

    Init dev tools

--- src/App.js
+++ src/App.js
@@ -6,13 +6,30 @@ const Pet = () => {
   ]);
 };
 
-const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
-  ]);
-};
+class App extends React.Component {
+  handleTitleClick() {
+    alert("you clicked on the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        { onClick: this.handleTitleClick },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Havanese"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
+    ]);
+  }
+}
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
